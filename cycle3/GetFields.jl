c35838287c7a20ba3ba46077a204a4745210a198
delbempedro <pedrodelbemo@gmail.com>
2024-10-24 20:38:56 -0300

    fix boundary conditions
    fix some boundary conditions

---
+++ cycle3/GetFields.jl
@@ -0,0 +1,48 @@
+"""
+  GetFields
+
+Returns value of Bx, By and Ez fields considering the boundary conditions.
+
+Dependencies:
+- 
+
+Since:
+- 09/2024
+
+Authors:
+- Pedro Calligaris Delbem <pedrodelbem@usp.br>
+"""
+module GetFields
+
+    export get_cBx, get_cBy, get_Ez
+
+    # returns value of cBx in the point (i,j)
+    function get_cBx(mesh::Matrix{Float64}, i::Int, j::Int, side::Int)
+
+        return mesh[i, j]
+
+    end
+
+    # returns value of cBy in the point (i,j)
+    function get_cBy(mesh::Matrix{Float64}, i::Int, j::Int, side::Int)
+
+        return mesh[i, j]
+
+    end
+
+    # returns value of Ez in the point (i,j)
+    function get_Ez(mesh::Matrix{Float64}, i::Int, j::Int, side::Int)
+
+        if  j <= 0 || j >= side || i <= side # boundary condition
+
+            return 0.0
+
+        else # normal condition
+
+            return mesh[i, j]
+
+        end
+
+    end
+
+end
--- cycle3/main.jl
+++ cycle3/main.jl
@@ -7,6 +7,7 @@ To run the main() function, call `julia main.jl` on the terminal.
 
 Dependencies:
 - Plots.jl
+- GetFields.jl
 
 Since:
 - 10/2024
@@ -16,40 +17,55 @@ Authors:
 """
 module cycle3
 
-  include("./BoundaryConditions.jl")
-  using .BoundaryConditions
+  include("./GetFields.jl")
+  using .GetFields
   using Plots
 
   function main()
 
   # defines phisicial consts
-  sc::Float64 = 0.45 #courant parameter (c*delta(t)/delta(x))
+  sc::Float64 = 0.5 #courant parameter (c*delta(t)/delta(x))
   er::Tuple{Float64, Float64} = (1.0, 10.0) #permittivity of free space
   E_initial::Float64 = 1.0 #initial E field
+  omega::Float64 = 10.0 #w*delta_t
 
   # defines initial and final time
   side::Int = 100 #mesh size
-  final_t::Int = 10000 #final time
+  time::Int = -1
+  final_t::Int = 100 #final time
+  
+  # initializes By times c field
+  cBy::Matrix{Float64} = zeros(side, side)
+  for i in 1:side cBy[1, i] = E_initial*cos(omega*time) end
 
   # initializes Ez field
+  time += 1
   Ez::Matrix{Float64} = zeros(side, side) 
-  for i in 1:side Ez[1, i] = 1.0 end
+  for i in 1:side Ez[1, i] = E_initial*cos(omega*time) end
 
-  cBy::Matrix{Float64} = zeros(side, side) # initializes By times c field
-  cBx::Matrix{Float64} = zeros(side, side) # initializes Bx times c field
+  # initializes Bx times c field
+  cBx::Matrix{Float64} = zeros(side, side)
 
   # time loop
-  for t in 0:final_t
+  for time in 0:final_t
 
     current_er = er[1]
 
-    if t % 2 == 0 # even time
+    if time % 2 == 0 # even time
 
+      
       # atualizes eletric field
-      for i in 1:side
-        for j in 1:side
-          if i >= 50 && i <= 75 && j >= 50 && j <= 75 current_er = er[2] end
-          Ez[i, j] += (sc / current_er) * ((BoundaryConditions.get_cBy(cBy, i+1, j, side) - BoundaryConditions.get_cBy(cBy, i-1, j, side)) - (BoundaryConditions.get_cBx(cBx, i, j+1, side) - BoundaryConditions.get_cBx(cBx, i, j-1, side)))
+      for i in 1:2:side
+        for j in 1:2:side
+
+          #if i >= 50 && i <= 75 && j >= 50 && j <= 75 current_er = er[2] end
+          if i == 1 # first line Ez
+            Ez[1, j] = E_initial*cos(omega*time)      
+          elseif j == 1 || i == side || j == side # boundary conditions
+            Ez[i, j] = 0.0
+          else # other cases
+            Ez[i, j] += (sc / current_er) * ((GetFields.get_cBy(cBy, i+1, j, side) - GetFields.get_cBy(cBy, i-1, j, side)) - (GetFields.get_cBx(cBx, i, j+1, side) - GetFields.get_cBx(cBx, i, j-1, side)))
+          end
 
         end
       end
@@ -57,12 +73,29 @@ module cycle3
     else # odd time
 
       # atualizes magnectic field
-      for i in 2:side
-        for j in 2:side
+      for i in 1:side
+        for j in 1:side
 
-            cBx[i, j] += sc * (BoundaryConditions.get_Ez(Ez, i, j+1, side) - BoundaryConditions.get_Ez(Ez, i, j-1, side))
-            cBy[i, j] -= sc * (BoundaryConditions.get_Ez(Ez, i+1, j, side) - BoundaryConditions.get_Ez(Ez, i-1, j, side))
-          
+          if i%2 == 1
+
+            if j%2 == 0
+
+              if i == 1
+                cBy[i, j] = E_initial*cos(omega*time)
+              else
+                cBy[i, j] += sc * (GetFields.get_Ez(Ez, i+1, j, side) - GetFields.get_Ez(Ez, i-1, j, side))
+              end
+
+            end
+
+          else
+
+            if j%2 == 0
+              cBx[i, j] += sc * (GetFields.get_Ez(Ez, i, j+1, side) - GetFields.get_Ez(Ez, i, j-1, side))
+            end
+
+          end
+
         end
       end
 
@@ -70,9 +103,9 @@ module cycle3
 
 
     # Plot
-    if t % 100 == 0
-      heatmap(Ez', title="Campo Elétrico - Passo $t", c=:inferno)
-      savefig("campo_eletrico_$t.png") # save the figure as a PNG file
+    if time % 10 == 0
+      heatmap(Ez', title="Campo Elétrico - Passo $time", c=:inferno)
+      savefig("campo_eletrico_$time.png") # save the figure as a PNG file
     end
 
   end
