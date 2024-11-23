cf1e5966e01c863d9e9642fe6b7c2aab8bfacc9f
delbempedro <pedrodelbemo@gmail.com>
2024-11-22 23:13:28 -0300

    create files
    create main and getcomponents

---
+++ cycle4/main.jl
@@ -0,0 +1,32 @@
+"""
+  cycle3
+
+Module that...
+
+To run the main() function, call `julia main.jl` on the terminal.
+
+Dependencies:
+- GetComponents
+
+Since:
+- 11/2024
+
+Authors:
+- Pedro Calligaris Delbem <pedrodelbem@usp.br>
+"""
+module cycle4
+
+"""  include("./Module.jl")
+  using .Module"""
+
+  function main()
+
+    t::Int = 0 #time
+    omega::Float64 = 0.9 #0.9c
+    R::Float64 = 1 #radius
+
+    
+
+  end
+
+end
